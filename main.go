3aca84cb203c13fdbc632dc27eabc6ddc90c0679
Aldric2023 <2020151626@ub.edu.bz>
2023-02-13 07:04:44 -0600

    wait group first update

--- main.go
+++ main.go
@@ -2,10 +2,36 @@
 
 package main
 
-import(
+import (
 	"fmt"
+	"sync"
 )
 
 func main(){
-	
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	func(){
+		defer wg.Done()
+		fmt.Println("hola")
+
+	}()
+
+
+	func(){
+		defer wg.Done()
+		fmt.Println("ni hao")
+
+	}()
+
+	func(){
+		defer wg.Done()
+		fmt.Println("hello")
+
+	}()
+
+	wg.Wait()
+
+
 }
